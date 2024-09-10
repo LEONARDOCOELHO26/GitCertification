286c4bf63982e1e913f2d781bac4595c8a9cfb3d
Leonardo A S Coelho <71470229+LEONARDOCOELHO26@users.noreply.github.com>
2024-09-10 17:12:44 -0300

    Add files via upload

---
+++ Resolvendo Códigos em Python com o Github Copilot/01.py
@@ -0,0 +1,9 @@
+# Recebe os dois dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados
+resultado = dado1 + " " + dado2
+
+# Imprime o resultado concatenado
+print(resultado)
